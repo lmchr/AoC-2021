a646c108ca46165d409fbef93293db8c29572eaa
larsmelcher <lars.melcher@web-computing.de>
2021-12-01 08:26:27 +0100

    d1p1

---
+++ aoc_1.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+let text = fs.readFileSync('aoc_1.txt')
+text = text.toString().split("\n").map(x => parseInt(x))
+res = []
+for (let i=0; i<text.length; i++){
+    if (i==0){
+        res.push(0)
+    } else {
+        res.push(text[i] > text[i - 1])
+    }
+}
+res = res.reduce((pv, cv) => pv + cv, 0);
+console.log(res);
