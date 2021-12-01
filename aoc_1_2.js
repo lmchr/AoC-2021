8f7b0d8b378cb3707dc245f001b0e2b2f9692c78
larsmelcher <lars.melcher@web-computing.de>
2021-12-01 12:07:35 +0100

    finish day 1

---
+++ aoc_1_2.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+let text = fs.readFileSync('aoc_1.txt')
+text = text.toString().split("\n").map(x => parseInt(x))
+res = []
+for (let i=0; i<text.length - 2; i++){
+    res.push(text[i] + text[i+1] + text[i + 2])
+}
+console.log(res)
+sum = 0;
+for (let index = 0; index < res.length - 1; index++) {
+    if (res[index + 1] > res[index]){
+        sum++;
+    }
+}
+console.log(sum);
